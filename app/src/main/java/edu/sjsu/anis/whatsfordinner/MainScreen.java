919585cd3bb1300e7a041ec4a2f44802464e7758
Anis Dhapa <anisdhapa11@gmail.com>
2018-02-08 09:48:36 -0800

    Pop Window Ready(need to change background color and add details)

--- app/src/main/java/edu/sjsu/anis/whatsfordinner/MainScreen.java
+++ app/src/main/java/edu/sjsu/anis/whatsfordinner/MainScreen.java
@@ -1,7 +1,10 @@
 package edu.sjsu.anis.whatsfordinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        ImageButton wfd = ( ImageButton)   (findViewById(R.id.mainScreen));
+
+
+        wfd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainScreen.this, pop.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/sjsu/anis/whatsfordinner/pop.java
@@ -0,0 +1,29 @@
+package edu.sjsu.anis.whatsfordinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by anisdhapa on 2/8/18.
+ */
+
+public class pop extends Activity{
+
+    //need to change background color for pop window
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popwindow);
+
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = (int) (dm.widthPixels * 0.6);
+        int height = (int) (dm.heightPixels * 0.4);
+
+        getWindow().setLayout(width ,height );
+    }
+}
