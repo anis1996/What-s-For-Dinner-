f10467865a42276c20b421e5c024200102c7adfe
Anis Dhapa <anisdhapa11@gmail.com>
2018-02-13 12:55:57 -0800

    add Recipe Class

---
+++ app/src/main/java/edu/sjsu/anis/whatsfordinner/Recipe.java
@@ -0,0 +1,69 @@
+package edu.sjsu.anis.whatsfordinner;
+import java.util.ArrayList;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by anisdhapa on 2/13/18.
+ */
+
+public class Recipe {
+    private Drawable image;
+    private String recipeName;
+    private ArrayList<String> ingredients;
+    private String direction;
+
+    public Recipe(String name, ArrayList<String> ingredients, String directions, Drawable img) {
+        this.recipeName = name;
+        this.ingredients = ingredients;
+        this.image= img;
+        this.direction = directions;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public String getDirection() { return direction; }
+
+    public void setRecipeNameName(String name) {
+        this.recipeName = name;
+    }
+
+    public void setIngred(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        return getRecipeName();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Recipe r = (Recipe) obj;
+        if (this.recipeName.equals(r.getRecipeName())) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+}
